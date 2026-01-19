5ba7b526b36724353775bac8b2cc4742d3ed72ef
Muhammad Rehman <muhammadrehman@Muhammads-MacBook-Pro.local>
2026-01-19 17:52:46 +0100

    Remove Comments

--- models/staging/sourcing/stg_card_segment.sql
+++ models/staging/sourcing/stg_card_segment.sql
@@ -1,8 +1,5 @@
 {{ config(materialized='view') }}
 
--- Passthrough staging view:
--- - keeps staging lightweight (no duplicate data in S3)
--- - enables tests/docs at a stable `stg_*` layer
 with card_segment as (
     select *
     from {{ source('sourcing', 'segments__public__card_segment') }}
--- models/staging/sourcing/stg_segments.sql
+++ models/staging/sourcing/stg_segments.sql
@@ -1,8 +1,5 @@
 {{ config(materialized='view') }}
 
--- Passthrough staging view:
--- - keeps staging lightweight (no duplicate data in S3)
--- - enables tests/docs at a stable `stg_*` layer
 with segments as (
     select *
     from {{ source('sourcing', 'segments__public__segments') }}
